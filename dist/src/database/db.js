a09ad9914456ec097dc935ad6a8298e8b4ea2d4a
ValentinaOrlas <johanavop25@gmail.com>
2024-08-20 10:26:04 -0500

    Removed admidEventos.db and admidEventos.db-journal

--- dist/src/database/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.db = void 0;
-const sqlite3_1 = require("sqlite3");
-exports.db = new sqlite3_1.Database('admidEventos.db', err => {
-    err ? console.log('Error en la conexión de la base de datos', err.message)
-        : console.log('Conexión a la bases de datos exitosa');
-});
