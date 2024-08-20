154eb82c3853af10fb835c36403cf215a5cf5263
ValentinaOrlas <johanavop25@gmail.com>
2024-08-20 10:18:48 -0500

    Agregando rutas a app

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
 import express from "express";
 import routeEventos from "./src/routes/routeEventos/routeEventos";
+import routeOrganizadores from './src/routes/routeOrganizadores/routeOrganizadores';
+import routeParticipantes from './src/routes/routePartcipantes/routeParticipantes';
 
 const app = express();
 const PORT = process.env.PORT || 10101;
 
 app.use(express.json());
-app.use("/", routeEventos);
 
-
+app.use('/', routeEventos);
+app.use('/',routeOrganizadores)
+app.use('/',routeParticipantes);
 
 app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`))
 .on("error", (error) => {
